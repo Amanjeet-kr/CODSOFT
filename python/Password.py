76007cad57a3e0de8ea94cc5ce883570ddfc3fdb
Amanjeet-kr <kumaramanjeet748@gmail.com>
2025-11-24 10:15:29 +0530

    Update Password.py

--- python/Password.py
+++ python/Password.py
@@ -2,9 +2,7 @@ import random
 import string
 
 def generate_password(length):
-    # All possible characters: letters, digits, punctuation
     characters = string.ascii_letters + string.digits + string.punctuation
-    # Randomly choose 'length' characters from the 'characters'
     password = ''.join(random.choice(characters) for _ in range(length))
     return password
 
